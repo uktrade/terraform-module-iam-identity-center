ab471006cf86991971f9887aa614f8d63555c45d
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2024-07-29 14:25:16 +0100

    Add resource for attaching AWS-managed policies.

--- permission-set/main.tf
+++ permission-set/main.tf
@@ -18,3 +18,25 @@ resource "aws_ssoadmin_permission_set_inline_policy" "inline_policy" {
   inline_policy = jsonencode(var.args.inline_policy)
 }
 
+## Attached AWS-managed Policies
+
+locals {
+    aws_managed_policies_map_by_name = {
+        for aws_managed_policy in try( var.args.aws_managed_policies, [] ) : aws_managed_policy.name => {
+            name = aws_managed_policy.name
+        }
+    }
+}
+
+data "aws_iam_policy" "aws_managed_policies" {
+    for_each = local.aws_managed_policies_map_by_name
+    name = each.value.name
+}
+
+resource "aws_ssoadmin_managed_policy_attachment" "aws_managed_policies" {
+    for_each = local.aws_managed_policies_map_by_name
+    instance_arn = var.identity_store.arns[0]
+    permission_set_arn = aws_ssoadmin_permission_set.permission_set.arn
+    managed_policy_arn = data.aws_iam_policy.aws_managed_policies[each.value.name].arn
+}
+
