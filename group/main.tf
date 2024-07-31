edb35c38fe83c0303ee6493bcade4c06471b5979
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2024-07-31 10:14:33 +0100

    Add resource to create IDC Group Entitlement.

--- group/main.tf
+++ group/main.tf
@@ -24,3 +24,54 @@ resource "aws_identitystore_group" "groups" {
     description = each.value.description
     identity_store_id = var.identity_store.identity_store_ids[0]
 }
+
+# Entitlements (aka Group-PermissionSet-Account mapping)
+
+locals {
+    # Change this map from being keyed by filename to keyed by permission_set.name, which is more useful here.
+    permission_set_map_by_name = {
+        for permission_set in var.permission_sets :
+            permission_set.permission_set.name => permission_set
+    }
+
+    # A map of account_sets, keyed by name.
+    account_set_map = {
+        for account_set in var.args.account_sets :
+            account_set.name => toset(account_set.accounts)
+    }
+
+    # Build a list of entitlements.
+    entitlement_list = flatten([
+        for role in var.args.roles : [
+            for permission_set in try( role.permission_sets, [] ) : [
+                for account_set in try( permission_set.account_sets, [] ) : [
+                    for account_set_accounts in local.account_set_map[account_set] : {
+                        team = var.args.team.name
+                        role = role.name
+                        account = account_set_accounts
+                        permission_set = permission_set.name
+                    }
+                ]
+            ]
+        ]
+    ])
+    # Create a map of entitlements from the list, keyed by "team_role_account" to be unique.
+    entitlement_map = {
+        for entitlement in local.entitlement_list : "${entitlement.team}_${entitlement.role}_${entitlement.permission_set}_${entitlement.account}" => {
+            group = "${entitlement.team}_${entitlement.role}"
+            account = entitlement.account
+            permission_set = entitlement.permission_set
+        }
+    }
+
+}
+
+resource "aws_ssoadmin_account_assignment" "entitlements" {
+    for_each = local.entitlement_map
+    instance_arn = var.identity_store.arns[0]
+    permission_set_arn = local.permission_set_map_by_name[each.value.permission_set].permission_set.arn
+    principal_id = aws_identitystore_group.groups[each.value.group].group_id # ID of the group created in this module and required for this entitlement.
+    principal_type = "GROUP"
+    target_id = var.aws_account_map[each.value.account] # The ID of the AWS account. Lookup the account id from the name in var.aws_account_map
+    target_type = "AWS_ACCOUNT"
+}
--- group/variables.tf
+++ group/variables.tf
@@ -7,3 +7,13 @@ variable "identity_store" {
   description = "AWS Identity Centre configuration."
   type = any
 }
+
+variable "permission_sets" {
+  description = "A map of outputs (resources) from the permission_set module."
+  type = map(any)
+}
+
+variable "aws_account_map" {
+  description = "A map of organisation accounts, keyed by name."
+  type = map(string)
+}
