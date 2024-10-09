8da2ca74ffb2a2f713247ffb1134174eaf1fac21
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2024-10-09 14:38:25 +0100

    Add Account functions.

--- scripts/idc_helper.py
+++ scripts/idc_helper.py
@@ -73,3 +73,87 @@ def get_permission_set_property(PermissionSetArn,property):
     logger.debug(f"permission_set: {permission_set}")
     return permission_set['PermissionSet'][property]
 
+
+def get_accounts():
+    account_dict = {}
+    accounts = org_client.list_accounts(MaxResults=20)
+    while True:
+        for account in accounts['Accounts']:
+            account_dict[account['Id']]=account['Name']
+        if "NextToken" in accounts:
+            accounts = org_client.list_accounts(MaxResults=20,NextToken=accounts["NextToken"])
+        else:
+            break
+    return account_dict
+
+def get_account_property(AccountId,property):
+    account = org_client.describe_account(
+        AccountId=AccountId
+    )
+    logger.debug(f"account: {account}")
+    return account['Account'][property]
+
+def get_account_assignments(permission_set, account_id):
+    account_assignments = []
+    next_token = ""
+    while True:
+        client_account_assignments = sso_client.list_account_assignments(
+            AccountId = account_id,
+            InstanceArn = client_sso_instance["InstanceArn"],
+            MaxResults=100,
+            NextToken=next_token,
+            PermissionSetArn = permission_set
+        )
+        logger.debug(f"client_account_assignments: {client_account_assignments}")
+        account_assignments.extend(client_account_assignments["AccountAssignments"])
+        if "NextToken" in client_account_assignments:
+            next_token=client_account_assignments["NextToken"]
+            logger.debug(f"next_token: {next_token}")
+        else:
+            logger.debug(f"account_assignments: {account_assignments}")
+            break
+    return account_assignments
+
+def get_account_assignments_for_principal(principal_id, principal_type):
+    account_assignments = []
+    client_account_assignments_for_principal = sso_client.list_account_assignments_for_principal(
+        InstanceArn = client_sso_instance["InstanceArn"],
+        MaxResults=100,
+        PrincipalId=principal_id,
+        PrincipalType=principal_type
+    )
+    logger.debug(f"client_account_assignments_for_principal: {client_account_assignments_for_principal}")
+    while True:
+        account_assignments.extend(client_account_assignments_for_principal["AccountAssignments"])
+        if "NextToken" in client_account_assignments_for_principal:
+            client_account_assignments_for_principal = sso_client.list_account_assignments_for_principal(
+                InstanceArn = client_sso_instance["InstanceArn"],
+                MaxResults=100,
+                NextToken=client_account_assignments_for_principal["NextToken"],
+                PrincipalId=principal_id,
+                PrincipalType=principal_type
+            )
+            logger.debug(f"client_account_assignments_for_principal: {client_account_assignments_for_principal}")
+        else:
+            logger.debug(f"account_assignments: {account_assignments}")
+            break
+    return account_assignments
+
+def get_permission_set_accounts(permission_set):
+    logger.debug(f"permission_set: {permission_set}")
+    accounts = []
+    next_token = ""
+    while True:
+        permission_set_accounts = sso_client.list_accounts_for_provisioned_permission_set(
+            InstanceArn=client_sso_instance["InstanceArn"],
+            MaxResults=100,
+            NextToken=next_token,
+            PermissionSetArn=permission_set
+            # ProvisioningStatus='LATEST_PERMISSION_SET_PROVISIONED'|'LATEST_PERMISSION_SET_NOT_PROVISIONED'
+        )
+        accounts.extend(permission_set_accounts["AccountIds"])
+        if "NextToken" in permission_set_accounts:
+            next_token=permission_set_accounts["NextToken"]
+        else:
+            return accounts
+
