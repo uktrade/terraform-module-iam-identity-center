ea44dc7539005a20c761e7d25bc3d8805f84ce0e
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2024-11-12 16:14:12 +0000

    Create a map of org OUs, keyed by name.

--- org/main.tf
+++ org/main.tf
@@ -16,3 +16,10 @@ data "aws_organizations_organizational_unit_descendant_accounts" "accounts" {
   parent_id = each.value
 }
 
+locals {
+  # Map of organisation OUs keyed by name.
+  org_ou_map = merge(
+    { "${data.aws_organizations_organization.org.roots[0].name}" = data.aws_organizations_organization.org.roots[0].id },
+    { for ou in data.aws_organizations_organizational_unit_descendant_organizational_units.ous.children : "${ou.name}" => ou.id }
+  )
+}
