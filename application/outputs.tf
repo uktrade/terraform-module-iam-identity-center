ea1e7d16e13e10f44108ccb52e2c1259035b7e64
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2024-11-26 14:01:21 +0000

    Create IDC SSO application module.

---
+++ application/outputs.tf
@@ -0,0 +1,3 @@
+output "sso_application" {
+    value = aws_ssoadmin_application.sso_application
+}
---
+++ application/variables.tf
@@ -0,0 +1,9 @@
+variable "args" {
+  description = "A map of Identity Centre applications."
+  type = any
+}
+
+variable "identity_store" {
+  description = "AWS Identity Centre configuration."
+  type = any
+}
